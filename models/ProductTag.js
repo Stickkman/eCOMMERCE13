6404c3aa122f29b13ba71ca32643df55afde5aa7
stickkman <stickkman@gmail.com>
2023-02-14 14:58:23 -0500

    coded productTag model to reqs

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {  // integer wo nulls as pk w auto increment
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: { // integer that refs product model's id
+      type: DataTypes.INTEGER,
+        references: { model: 'product', key: 'id' }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, // integer that refs tag model's id
+        references: { model: 'tag', key: 'id' }
+    }
   },
   {
     sequelize,
