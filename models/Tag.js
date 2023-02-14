c4866ca06d9450ea4745bbaef921e2673d95f86f
stickkman <stickkman@gmail.com>
2023-02-14 14:48:47 -0500

    coded tag model to reqs

--- models/Tag.js
+++ models/Tag.js
@@ -7,11 +7,20 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: { // integer wo nulls, pk, uses auto increment
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: { // string type
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
     timestamps: false,
-    freezeTableName: true,
+    freezeTableName: true, // prevents model name change to plural
     underscored: true,
     modelName: 'tag',
   }
