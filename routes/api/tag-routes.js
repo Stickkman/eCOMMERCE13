cce7765de4d6a24f05e0cb1e1d83bec5f35f2223
stickkman <stickkman@gmail.com>
2023-02-15 13:02:00 -0500

    coded findall and findone tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,28 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-});
+  try {
+    const allTags = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag, },],
+      });
+      res.json(allTags);
+    } catch (e) { console.log(e); res.status(500).json(e);}
+    });
 
-router.get('/:id', (req, res) => {
+
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try { 
+    const singleTag = await Tag.findOne({ where: { id: req.params.id, },
+    include: [{ model: Product, through: ProductTag, }, ],
+    });
+    if(!singleTag) { res.status(404).json({ message: 'Tag Id Not Found'}); return;}
+    res.json(singleTag);
+  } catch (e) { console.log(e); res.status(500).json(e);}
 });
 
 router.post('/', (req, res) => {
